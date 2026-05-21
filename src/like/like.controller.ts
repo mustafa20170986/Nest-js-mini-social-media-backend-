2dddfafc3fcce0a2ad8d4aee8fb1e04b0f838ef3
mustafa20170986 <jster0986@gmail.com>
2026-05-22 01:48:00 +0600

    who liked your post

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { LikeService } from './like.service';
 
 @Controller('like')
@@ -8,5 +8,9 @@ export class LikeController {
   @Post('dolike/:postId/:userId')
   likepost(@Param('postId') postId: string, @Param('userId') userId: string) {
     return this.likeService.likepost(postId, userId);
+  }
+  @Get('wholiked/:postId')
+  findlikeduser(@Param('postId') postId: string) {
+    return this.likeService.findlikeduser(postId);
   }
 }
--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { likeSchema } from 'src/schema/like.schema';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 @Injectable()
 export class LikeService {
   constructor(
@@ -24,5 +24,51 @@ export class LikeService {
       const delike = await this.likeModel.deleteOne({ _id: likexist._id });
       return delike;
     }
+  }
+  //get who liked
+  //explicitly mention the colllectionname in the schema file
+  async findlikeduser(postId: string) {
+    // const findpost = new Types.ObjectId(postId);
+    const result = await this.likeModel
+      .aggregate([
+        {
+          $match: { postId: postId },
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'userId',
+            foreignField: '_id',
+            as: 'userdetails',
+          },
+        },
+        {
+          $unwind: '$userdetails',
+        },
+        {
+          $group: {
+            _id: '$postId',
+            totalLikes: { $sum: 1 },
+            likedBy: {
+              $push: {
+                userId: '$userId',
+                name: '$userdetails.name',
+              },
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            totalLikes: 1,
+            likedBy: 1,
+          },
+        },
+      ])
+      .exec();
+    if (!result || result.length === 0) {
+      return { message: 'no likes yet' };
+    }
+    return result[0];
   }
 }
